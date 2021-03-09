c511e862cdc308accd9ea5b0eed947cfdfdf18fa
arybowo <bowoary742@gmail.com>
2021-03-09 22:31:25 +0700

    halaman data mahasiswa

---
+++ page/data.php
@@ -0,0 +1,93 @@
+<?php
+//memasukkan file koneksi.php
+include('../assets/helper/koneksi.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	
+</head>
+<body>
+	<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #4B0082;">
+		<div class="container">
+			<a class="navbar-brand" href="#">BOOT CAMP CC</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+ 
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item active">
+						<a class="nav-link" href="data.php">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="../assets/helper/tambah.php">Tambah</a>
+					</li>
+				</ul>
+			</div>
+			<a href="../assets/helper/logout.php" class="btn btn-sm btn-danger" role="button">Logout</a>
+		</div>
+	</nav>
+	
+	<div class="container" style="margin-top:20px">
+		<h2 align="center">DATA MAHASISWA</h2>
+		
+		<hr>
+		
+		<table class="table table-striped table-hover table-sm table-bordered">
+			<thead class="thead-dark">
+				<tr>
+					<th>NO.</th>
+					<th>NAMA MAHASISWA</th>
+					<th>NPM</th>
+					<th>ALAMAT</th>
+					<th>AKSI</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php
+				//query ke database SELECT tabel mahasiswa urut berdasarkan id yang paling besar
+				$sql = mysqli_query($koneksi, "SELECT * FROM mahasiswa ORDER BY id DESC") or die(mysqli_error($koneksi));
+				//jika query diatas menghasilkan nilai > 0 maka menjalankan script di bawah if...
+				if(mysqli_num_rows($sql) > 0){
+					//membuat variabel $no untuk menyimpan nomor urut
+					$no = 1;
+					//melakukan perulangan while dengan dari dari query $sql
+					while($data = mysqli_fetch_assoc($sql)){
+						//menampilkan data perulangan
+						echo '
+						<tr>
+							<td>'.$no.'</td>
+							<td>'.$data['nama'].'</td>
+							<td>'.$data['npm'].'</td>
+							<td>'.$data['alamat'].'</td>
+							<td>
+								<a href="../assets/helper/edit.php?id='.$data['id'].'" class="badge badge-info">Edit</a>
+								<a href="../assets/helper/delete.php?id='.$data['id'].'" class="badge badge-danger" onclick="return confirm(\'Yakin ingin menghapus data ini?\')">Delete</a>
+							</td>
+						</tr>
+						';
+						$no++;
+					}
+				//jika query menghasilkan nilai 0
+				}else{
+					echo '
+					<tr>
+						<td colspan="6">Tidak ada data.</td>
+					</tr>
+					';
+				}
+				?>
+			<tbody>
+		</table>
+		
+	</div>
+	
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	
+</body>
+</html>
